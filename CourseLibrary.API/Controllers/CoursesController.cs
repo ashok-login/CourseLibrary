c7f1beee47273d1746b86ae1826143ab8629fbeb
Ashok Kumar <kak.mca@hotmail.com>
2020-02-28 11:07:56 +0530

    New Learnings

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -25,7 +25,7 @@ namespace CourseLibrary.API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<CourseDto>> GetCoursesForAuthor(Guid authorId)
         {
-            throw new Exception("Terrible error occurred");
+            //throw new Exception("Terrible error occurred");
             if(!_courseLibraryRepository.AuthorExists(authorId))
             {
                 return NotFound();
@@ -35,7 +35,7 @@ namespace CourseLibrary.API.Controllers
             return Ok(_mapper.Map<IEnumerable<CourseDto>>(coursesForAuthorFromRepo));
         }
 
-        [HttpGet("{courseId}")]
+        [HttpGet("{courseId}", Name = "GetCourseForAuthor")]
         public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
         {
             if(!_courseLibraryRepository.AuthorExists(authorId))
@@ -57,5 +57,22 @@ namespace CourseLibrary.API.Controllers
         //        return NotFound();
         //    }
         //}
+
+        public ActionResult<CourseDto> CreateCourseForAuthor(Guid authorId, CourseForCreationDto course)
+        {
+            if(!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var courseEntity = _mapper.Map<Entities.Course>(course);
+            _courseLibraryRepository.AddCourse(authorId, courseEntity);
+            _courseLibraryRepository.Save();
+            var courseToReturn = _mapper.Map<CourseDto>(courseEntity);
+            return CreatedAtRoute(
+                "GetCourseForAuthor",
+                new { authorId = authorId, courseId = courseToReturn.Id },
+                courseToReturn);
+        }
     }
 }
